428c4ba565b16ef5fd692cb4fe943c2b0bda8743
Steve Miner <steveminer@gmail.com>
2013-10-17 16:13:06 -0400

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velisco/herbert "0.5.4-SNAPSHOT"
+(defproject com.velisco/herbert "0.5.4"
   :description "A schema for edn"
   :url "https://github.com/miner/herbert"
   :license {:name "Eclipse Public License"
