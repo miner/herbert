009a5a8669f5cb4617134603f10db36832b52578
Steve Miner <steveminer@gmail.com>
2013-11-03 12:51:12 -0500

    clean up project.clj

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [squarepeg "0.6.1"]]
-  :profiles {:snapshot {:dependencies [[org.clojure/clojure "1.6.0-master-snapshot"]]}
-             :alpha {:dependencies [[org.clojure/clojure "1.6.0-alpha1"]]}
-             }
-  :repositories [["sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}]])
+  :profiles {:snapshot {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :alpha {:dependencies [[org.clojure/clojure "1.6.0-alpha1"]]} }
+  :repositories [["sonatype-public" {:url "https://oss.sonatype.org/content/groups/public/"}]])
 
