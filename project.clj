7dac3e3c19ca3cb02ae799730556cec603625eb0
Steve Miner <steveminer@gmail.com>
2013-12-06 18:11:16 -0500

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velisco/herbert "0.5.14-SNAPSHOT"
+(defproject com.velisco/herbert "0.5.15-SNAPSHOT"
   :description "A schema for edn"
   :url "https://github.com/miner/herbert"
   :license {:name "Eclipse Public License"
