df05cf6f22c7bea8ab34fc1e9d0101f33355fc28
Steve Miner <steveminer@gmail.com>
2013-12-12 16:42:16 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velisco/herbert "0.5.13-SNAPSHOT"
+(defproject com.velisco/herbert "0.5.13"
   :description "A schema for edn"
   :url "https://github.com/miner/herbert"
   :license {:name "Eclipse Public License"
@@ -7,5 +7,5 @@
                  [com.velisco/tagged "0.3.4"]
                  [squarepeg "0.6.1"]]
   :profiles {:snapshot {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
-             :alpha {:dependencies [[org.clojure/clojure "1.6.0-alpha2"]]} }
+             :alpha {:dependencies [[org.clojure/clojure "1.6.0-alpha3"]]} }
   :repositories [["sonatype-public" {:url "https://oss.sonatype.org/content/groups/public/"}]])
