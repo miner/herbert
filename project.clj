2d65dcc52066b2921df68aa4e56abff205102643
Steve Miner <steveminer@gmail.com>
2015-03-18 15:55:24 -0400

    updated libs

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.velisco/tagged "0.3.4"]
-                 [org.clojure/math.combinatorics "0.0.8"]
-                 [org.clojure/test.check "0.6.2"]
+                 [com.velisco/tagged "0.3.7"]
+                 [org.clojure/math.combinatorics "0.1.0"]
+                 [org.clojure/test.check "0.7.0"]
                  [squarepeg "0.6.1"]]  )
 
