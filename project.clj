eb54611b0761aadb732bc70896acd8fd95442567
Steve Miner <steveminer@gmail.com>
2015-09-14 09:31:14 -0400

    test.check update

--- project.clj
+++ project.clj
@@ -8,6 +8,6 @@
                  [com.velisco/tagged "0.3.7"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  ;; [org.clojure/test.check "0.7.0"]
-                 [org.clojure/test.check "0.8.0-RC1"]
+                 [org.clojure/test.check "0.8.2"]
                  [squarepeg "0.6.1"]]  )
 
