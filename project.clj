1e088284750b903bc6009f713b76a1ad151398f0
Steve Miner <steveminer@gmail.com>
2022-02-16 15:14:40 -0500

    update libs

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories {"releases" :clojars}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.velisco/tagged "0.4.1"]
-                 [org.clojure/math.combinatorics "0.1.1"]
-                 [org.clojure/test.check "0.9.0"]
+  :dependencies [[org.clojure/clojure "1.11.0-rc1"]
+                 [com.velisco/tagged "0.5.0"]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/test.check "1.1.1"]
                  [squarepeg "0.6.1"]]  )
 
